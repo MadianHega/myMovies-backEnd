28719e2214da5ba7bbe8fd264f5f9df45f61c889
MadianHega <madianhega@gmail.com>
2019-02-18 12:10:12 +0100

    [FEAT] : ajout de la function currentDate

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+// return la date d'aujourd'hui au format anglophone
+var currentDate = function () {
+  let newDate = new Date()
+  let year = newDate.getFullYear()
+  let month = newDate.getMonth() + 1
+  let day = newDate.getDate()
+  let date = year + "-" + month + "-" + day
+  return date
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
